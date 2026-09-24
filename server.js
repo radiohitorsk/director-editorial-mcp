bdcf88c69908cd2fb053dced52e33e78a26a3aad
radiohitorsk <radiohitorsk@gmail.com>
2026-09-24 16:41:37 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
-import {createServer} from "node": http;
+import {createServer} from "node:http"; 
 import {createMcpHandler, McpServer} from "@modelcontextprotocol/server";
 import {toNodeHandler} from "@modelcontextprotocol/node";
+import * as z from 'zod/v4';
+const handler=createMcpHandler (=>{
+  constsever=new McpServer
+  name:"мой-mcp",
+  version:"0.0.1"});
